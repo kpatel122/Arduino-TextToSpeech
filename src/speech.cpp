f007b24ecf3b70856400380fd1c2c25a3c0fd7b5
Kunal <kunal>
2019-03-04 11:28:27 +0000

    initial commit

---
+++ src/speech.cpp
@@ -0,0 +1,76 @@
+/*
+Speech.cpp - A library for speech synthesis with a XFS5152CE
+Copyright (c) 2019 Kunal Patel
+
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without
+restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following
+conditions:
+
+This permission notice shall be included in all copies or 
+substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+
+#include "Arduino.h"
+#include "Speech.h"
+
+ 
+
+CSpeech::CSpeech(int rx,int tx):ss(rx,tx)
+{
+  options = "[x0][t6][v5][s6][m51][g2][h2][n1]";
+  ss.begin(9600);
+}
+
+void CSpeech::speak(String message)
+{
+  
+  messageString =options + message;
+
+  // Length (with one extra character for the null terminator)
+  stringLength = messageString.length() + 1; 
+
+  // Copy it over 
+  messageString.toCharArray(messageBuffer, stringLength);
+  sendMessagetoSpeechModule();
+}
+
+void CSpeech::sendMessagetoSpeechModule()
+{
+  ss.write(0xFD);
+  ss.write((byte)0x0);
+  ss.write(2 + strlen(messageBuffer));
+  ss.write(0x01);
+  ss.write((byte)0x0);
+  ss.write(messageBuffer);
+}
+
+void CSpeech::waitForSpeech(unsigned long timeout = 60000) {
+  unsigned long start = millis();
+  bool done = false;
+  while ( ! done && (millis() - start) < timeout ) {
+    while ( ss.available() ) {
+      if ( ss.read() == 0x4F ) {
+        done = true;
+        break;
+      }
+    }
+  }
+}
+
+
+
---
+++ src/speech.h
@@ -0,0 +1,56 @@
+/*
+Speech.h - A library for speech synthesis with a XFS5152CE
+Copyright (c) 2019 Kunal Patel
+
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without
+restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following
+conditions:
+
+This permission notice shall be included in all copies or 
+substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+#ifndef SPEECH_H
+#define SPEECH_H
+
+#include "Arduino.h"
+#include <SoftwareSerial.h>
+
+
+
+#define MAX_SPEECH_CHAR 256
+
+class CSpeech
+{
+  public:
+    CSpeech(int rx,int tx);
+    void speak(String message);
+    void waitForSpeech(unsigned long timeout);
+  
+  private:
+    void sendMessagetoSpeechModule();
+
+    SoftwareSerial ss; // rx, tx
+    char messageBuffer[MAX_SPEECH_CHAR];
+    String incoming;
+    String options;
+    String messageString;
+    int stringLength;
+    
+};
+
+#endif
